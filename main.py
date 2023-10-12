a69ccfb7a82a6deed310dc3ca1c9c159266a4201
Daniele Cursano <daniele@Air-di-Daniele.lan>
2023-10-12 17:45:10 +0200

    debug main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.server import Server
 from src.structs import Sheet
+import sys
 
 if __name__=='__main__':
     app = Server()
-    app.run(debug=1)
+    debug = int(sys.argv[1]) if len(sys.argv) > 1 else 1
+    app.run(debug=debug)
